691ff24fe526a4d21adac36ebdce24aa71ac0ff3
Ensar <ensarhamzic01@gmail.com>
2023-06-29 01:39:27 +0200

    change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  api_url: 'https://localhost:7226/api',
+  // api_url: 'https://localhost:7226/api',
+  api_url: 'ttp://ensarhamzic-001-site1.dtempurl.com/api',
 };
