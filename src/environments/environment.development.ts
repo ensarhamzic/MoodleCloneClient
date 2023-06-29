25d025781a221eadca4eb120a739cc9aa256712e
Ensar <ensarhamzic01@gmail.com>
2023-06-29 02:05:50 +0200

    nova ruta

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // api_url: 'https://localhost:7226/api',
-  api_url: 'ttp://ensarhamzic-001-site1.dtempurl.com/api',
+  api_url: 'https://ekilo.bsite.net/api',
 };
